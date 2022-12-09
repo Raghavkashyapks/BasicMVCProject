baea64e67a851378fe8c16c28fb2311ba762b77b
Raghav Kashyap <Raghav.Kashyap@sstech.us>
2022-12-09 17:19:13 +0530

    Initial commit

---
+++ BasicMVCProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BasicMVCProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
